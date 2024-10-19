7ab57391aaad70421c75cc624a88549fbad14bd5
Jalamaker <95651109+Jalamaker@users.noreply.github.com>
2024-10-19 15:14:59 +0300

    Update VOEnergyBoomEffect.java

--- src/VO/entities/VOEnergyBoomEffect.java
+++ src/VO/entities/VOEnergyBoomEffect.java
@@ -1,7 +1,9 @@
 package VO.entities;
 
 import arc.graphics.Color;
+import arc.graphics.g2d.Draw;
 import arc.graphics.g2d.Lines;
+import arc.math.Angles;
 import arc.math.Interp;
 import mindustry.entities.Effect;
 import mindustry.graphics.Drawf;
@@ -13,7 +15,8 @@ public class VOEnergyBoomEffect extends Effect{
     public Color color = Color.lightGray, midColor = Color.white, waveColorFrom = Color.white, waveColorTo = Color.white;
     public float width = 5f, length = 15f, midWidth = 2.5f, midLength = 5f;
     public float waveSize = 12f, waveStroke = 3f;
-    public float baseRotation = 0f;
+    public float baseRotation = 0f, polygonRotation;
+    public int sides = -1;
     public Interp interp = Interp.linear, waveInterp = Interp.pow3Out;
     public boolean interpStroke = true;
     
@@ -24,12 +27,24 @@ public class VOEnergyBoomEffect extends Effect{
 
     public void render(EffectContainer e){
         if(waveSize > 0 && waveStroke > 0){
-            color(waveColorFrom, waveColorTo, e.fin(waveInterp));
+            /*color(waveColorFrom, waveColorTo, e.fin(waveInterp));
             if(interpStroke = true){
                 stroke(waveInterp.apply(waveStroke, 0, e.fin()));
             } else stroke(Interp.linear.apply(waveStroke, 0, e.fin()));
             float circleRad = waveInterp.apply(0, waveSize, e.fin());
-            Lines.circle(e.x, e.y, circleRad);
+            Lines.circle(e.x, e.y, circleRad);*/
+
+            float fin = e.fin();
+            float ifin = e.fin(interp);
+            float ox = e.x + Angles.trnsx(e.rotation, 0, 0), oy = e.y + Angles.trnsy(e.rotation, 0, 0);
+
+            Draw.color(waveColorFrom, waveColorTo, ifin);
+            Lines.stroke(interp.apply(waveStroke, 0, fin));
+
+            float rad = interp.apply(0, waveSize, fin);
+            Lines.poly(ox, oy, sides <= 0 ? Lines.circleVertices(rad) : sides, rad, polygonRotation + e.rotation);
+
+            Drawf.light(ox, oy, rad * 3, Draw.getColor(), 0.8f * e.fin(waveInterp));
         }
 
         if(width > 0 && length > 0){
