a87aeb3afc2ba2e4ba243d1587f52cddcdf5d60d
Jalamaker <95651109+Jalamaker@users.noreply.github.com>
2024-10-23 17:41:33 +0300

    Update VOUnitChanges.java

--- src/VO/VOUnitChanges.java
+++ src/VO/VOUnitChanges.java
@@ -546,14 +546,14 @@ public class VOUnitChanges {
         UnitType anthicus = UnitTypes.anthicus.weapons.get(0).bullet.spawnUnit;
         anthicus.deathExplosionEffect = anthicus.weapons.get(0).bullet.hitEffect = anthicus.weapons.get(0).bullet.despawnEffect = Fx.none;
         anthicus.weapons.get(0).bullet.shootEffect = new MultiEffect(new Effect(30, e -> {
-            color(Pal.missileYellow);
+            color(Pal.techBlue);
             e.scaled(6, i -> {
                 stroke((wstroke(140)) * i.fout());
                 Lines.circle(e.x, e.y, 2f + i.fin() * 25f);
             });
-            color(Pal.missileYellowBack);
-            e.scaled(20, i -> {stroke(i.fout() * 1.25f);
-                randLenVectors(e.id + 1, 7, 2f + 29f * i.finpow(), (x, y) -> {
+            color(Pal.techBlue);
+            e.scaled(20, i -> {stroke(i.fout() * 1.5f);
+                randLenVectors(e.id + 1, 8, 5f + 24f * i.finpow(), (x, y) -> {
                     lineAngle(e.x + x, e.y + y, Mathf.angle(x, y), 1f + i.fout() * 3.5f);
                 });
             });
