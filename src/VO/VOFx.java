be300412eef592ab1947ca5088a2038115d5abf1
Jalamaker <95651109+Jalamaker@users.noreply.github.com>
2024-11-08 18:18:41 +0200

    Update VOFx.java

--- src/VO/VOFx.java
+++ src/VO/VOFx.java
@@ -57,26 +57,29 @@ public class VOFx {
         Drawf.tri(e.x, e.y, w, 4f * e.fout(), e.rotation + 180f);
     }),
 
-    greenBombPlus = new Effect(60f, 100f, e -> {
-        float circleRad = Interp.pow5Out.apply(5f, 80f, e.fin());
-        e.scaled(120f, i -> {color(Pal.heal.cpy().a(0.3f), Pal.heal.cpy().a(0f), i.fin());
-            Fill.circle(e.x, e.y, circleRad);
+    greenBombPlus = new Effect(120f, 180f, e -> {
+        float fillCircleRad = Interp.pow10Out.apply(5f, 80f, e.fin());
+        color(Pal.heal.cpy().a(0.15f), Pal.heal.cpy().a(0f), e.fin());
+        Fill.circle(e.x, e.y, fillCircleRad);
+        
+        e.scaled(60f, i -> {
+            float circleRad = Interp.pow5Out.apply(5f, 80f, i.fin());
+            color(Pal.heal);
+            stroke(Interp.pow3In.apply(2.5f, 0, i.fin()));
+            Lines.circle(e.x, e.y, circleRad);
+
+            Drawf.light(e.x, e.y, circleRad * 2, Pal.heal, 0.8f * i.fin(Interp.pow10Out));
+
+            float spikeLen = Interp.pow3In.apply(110f, 0f, i.fin());
+            for(int ii = 0; ii < 4; ii++){
+                Drawf.tri(e.x, e.y, 7f, spikeLen, ii*90);
+            }
+
+            color(Color.white);
+            for(int ii = 0; ii < 4; ii++){
+                Drawf.tri(e.x, e.y, 4f, spikeLen * (80/180), ii*90);
+            }
         });
-        color(Pal.heal);
-        stroke(Interp.pow3In.apply(2.5f, 0, e.fin()));
-        Lines.circle(e.x, e.y, circleRad);
-
-        Drawf.light(e.x, e.y, circleRad * 2, Pal.heal, 0.8f * e.fin(Interp.pow10Out));
-
-        float spikeLen = Interp.pow3In.apply(110f, 0f, e.fin());
-        for(int i = 0; i < 4; i++){
-            Drawf.tri(e.x, e.y, 7f, spikeLen, i*90);
-        }
-
-        color(Color.white);
-        for(int i = 0; i < 4; i++){
-            Drawf.tri(e.x, e.y, 4f, spikeLen * (80/180), i*90);
-        }
 
         e.scaled(40f, i -> {
             float sparkLen = Interp.pow3Out.apply(0f, 95f, i.fin());
